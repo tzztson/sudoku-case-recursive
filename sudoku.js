f4d278507e9aaf0e912f059df96e2ddba6e88825
tzztson <zhenghu61919@gmail.com>
2022-10-15 01:08:39 +0800

    SUDOKU CASES for SUDOKU players

---
+++ sudoku.js
@@ -0,0 +1,25 @@
+const num = new Array();
+
+const arr_sum = (arr) => {
+  return arr.reduce((pre, cur) => {
+    pre+= cur;
+    return pre;
+  }, 0)
+}
+
+const sudoku = (sum, cnt, c, lim) => {
+  if (cnt == lim){
+    if (sum == arr_sum(num)){
+      console.log(`${sum} = ${num}`);
+    }
+    return;
+  }
+  for (let i = c; i < 10; i++){
+    num[cnt] = i;
+    sudoku(sum, cnt+1, i+1, lim);
+  }
+}
+
+for (let l = 1; l < 10; l ++)
+  for (let k = 1; k <= 45; k ++)
+    sudoku(k,0,1,l);
